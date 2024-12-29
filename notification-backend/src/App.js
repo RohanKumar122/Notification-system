8a3f4eda7b9c10946dae6b851cc95a2aa99594f4
Rohan Kumar <rk399504@gmail.com>
2024-12-29 22:09:16 +0530

    Added Register and login

--- notification-backend/src/App.js
+++ notification-backend/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
+import RegisterForm from './pages/Register';
+import LoginForm from './pages/login';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/login" element={<h1>login</h1>} />
+      <Route path="/register" element={<RegisterForm />} />
+      <Route path="/login" element={<LoginForm />} />
     </Routes>
   );
 }
